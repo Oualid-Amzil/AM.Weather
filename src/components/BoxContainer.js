ca80a8a56bf9cf65f1fcae025a0496e62cf40227
Oualid-Amzil <raskolanikouf2@gmail.com>
2023-11-10 10:23:10 +0100

    left side code

---
+++ src/components/BoxContainer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import cloudImg from "../images/michael-diane-weidner-h-rP5KSC2W0-unsplash.jpg";
+import { BsFillCloudsFill } from "react-icons/bs";
+
+import "./BoxContainer.css";
+
+const BoxContainer = () => {
+  return (
+    <div className="boxcontainer">
+      <div className="left__side">
+        <span className="title">AM.Weather</span>
+        <img src={cloudImg} alt="weather" />
+        <div className="degree">
+          <h1>16</h1>
+          <div className="info">
+            <div className="city__date">
+              <h2>London</h2>
+              <h5>06:09 - Monday, 9 Sep '19</h5>
+            </div>
+            <div className="weather__icon">
+              <BsFillCloudsFill className="cloud" />
+              <span>cloud</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="right__side"></div>
+    </div>
+  );
+};
+
+export default BoxContainer;
