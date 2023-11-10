bafa6e346d51eed5028ccdc2cd72cb6d26641665
Oualid-Amzil <raskolanikouf2@gmail.com>
2023-11-10 16:19:53 +0100

    add an image and make it blur

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import BoxContainer from "./components/BoxContainer";
+import hillImg from "./images/neom-YeLs9lJDx9M-unsplash.jpg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <BoxContainer />
+    <div
+      className="App"
+      style={{
+        background: `url(${hillImg}) center no-repeat`,
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="wrapper">
+        <BoxContainer />
+      </div>
     </div>
   );
 }
